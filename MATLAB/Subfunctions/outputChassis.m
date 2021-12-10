b41499e3cb7984b237d1c380ce81e557929fdf60
Munir Alsafi <malja051@uottawa.ca>
2021-12-10 02:09:54 -0500

    pre merging solidworks

---
+++ MATLAB/Subfunctions/outputChassis.m
@@ -0,0 +1,14 @@
+% =========================================================================
+%   Function: 
+%
+%   Parameters: 
+%   
+%   Outputs: 
+%
+%   Description:
+% =========================================================================
+function SpringCalculation(springRate, frontpushrodforce)
+
+    shaft_file = 'H:\\groupFSAE2\\SolidWorks\\Equations\\shaft.txt';
+
+end
